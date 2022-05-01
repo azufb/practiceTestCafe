3f96c92d90d6304e82b1cab633f72ba4b1092031
azufb <curiousazunas@gmail.com>
2022-05-01 18:39:33 +0900

    サンプルテスト修正

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,9 @@
 import { Selector } from "testcafe";
 
-fixture`Getting Started`.page`https://devexpress.github.io/testcafe/example`;
+fixture`Getting Started`.page`http://localhost:8080`;
 
 test('my_first_test', async t => {
-    await t.expect(Selector('h1').withText('Example').exists).ok();
+    const text = await Selector('h1').innerText;
+    console.log(text);
+    await t.expect(text).eql("React TypeScript");
 });
