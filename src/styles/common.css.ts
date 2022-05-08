8b0a9dab632fb5b861f60afc2b5e62c36606eaf1
azufb <curiousazunas@gmail.com>
2022-05-08 20:36:50 +0900

    共通のスタイルまとめる

---
+++ src/styles/common.css.ts
@@ -0,0 +1,25 @@
+import { style } from '@vanilla-extract/css';
+
+export const wrapper = style({
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+});
+
+export const title = style({
+    textAlign: 'center'
+});
+
+export const contents = style({
+    width: '50vw',
+    margin: '0 auto'
+});
+
+export const footer = style({
+    marginTop: 'auto',
+    textAlign: 'center',
+});
+
+export const footerText = style({
+    marginBottom: '0.5rem'
+});
