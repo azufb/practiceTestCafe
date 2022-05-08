b94d0cc4e638917c999c77c25719e189b0acdd9d
azufb <curiousazunas@gmail.com>
2022-05-08 22:27:05 +0900

    ボタンスタイル修正

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Modal } from './Modal';
-import { buttons } from '../styles/buttons.css';
+import { buttons, button, button1, button2, button3 } from '../styles/buttons.css';
 import { contents } from '../styles/common.css';
 
 export const Buttons = () => {
@@ -15,9 +15,9 @@ export const Buttons = () => {
             <div className={contents}>
                 <h1>This is Buttons!</h1>
                 <div className={buttons}>
-                    <button>ボタン1</button>
-                    <button onClick={switchShowModal}>ボタン2</button>
-                    <button>ボタン3</button>
+                    <button className={`${button} ${button1}`}>ボタン1</button>
+                    <button onClick={switchShowModal} className={`${button} ${button2}`}>ボタン2</button>
+                    <button className={`${button} ${button3}`}>ボタン3</button>
                 </div>
                 <Modal show={show} setShow={setShow} />
             </div>
--- src/styles/buttons.css.ts
+++ src/styles/buttons.css.ts
@@ -4,3 +4,22 @@ export const buttons = style({
     display: 'flex',
     justifyContent: 'space-between',
 });
+
+export const button = style({
+    width: '30%',
+    padding: '0.5rem 0'
+});
+
+export const button1 = style({
+    backgroundColor: '#1274CC',
+    color: '#FFFFFF'
+});
+
+export const button2 = style({
+    backgroundColor: '#0CB981',
+    color: '#FFFFFF'
+});
+
+export const button3 = style({
+    backgroundColor: '#F2F9FF'
+})
