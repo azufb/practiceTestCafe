0296ce2e977f986aabc652aa7a8907ae921864ff
azufb <curiousazunas@gmail.com>
2022-05-08 19:24:57 +0900

    タイトルスタイル調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Routing } from './components/Routing';
+import { title } from './styles/title.css';
+
 export const App = () => {
     return (
         <div>
+            <h1 className={title}>Test Caf&eacute;を使ってみる！</h1>
             <Routing />
         </div>
     )
