5cb38852176266a9e464d257957dc8e1c7457077
azufb <curiousazunas@gmail.com>
2022-05-09 20:51:05 +0900

    スクリーンショット周りの設定

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,23 @@
 import { Selector } from "testcafe";
 
-fixture`Getting Started`.page`http://localhost:8080`;
+const fixtureName = 'Getting Started';
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth();
+const date = today.getDate();
+const hour = today.getHours();
+const minute = today.getMinutes();
+const second = today.getSeconds();
+const dateDirectoryName = `${year}-${month}-${date}_${hour}-${minute}-${second}`;
+
+fixture`${fixtureName}`.page`http://localhost:8080`;
 
 test('my_first_test', async t => {
     const text = await Selector('h1').innerText;
-    console.log(text);
-    await t.expect(text).eql("React TypeScript");
+    await t.expect(text).eql("Test Caféを使ってE2Eテストしてみる!");
+
+    await t.takeScreenshot({
+        path: `${dateDirectoryName}/my_first_test.png`,
+        fullPage: true
+    });
 });
