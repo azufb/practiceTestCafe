111195f5702a2340757fc70300386219dcb96d14
azufb <curiousazunas@gmail.com>
2022-05-10 20:35:33 +0900

    export方法修正

--- tests/testCode/home/checkHomeTabContents.ts
+++ tests/testCode/home/checkHomeTabContents.ts
@@ -1,6 +1,6 @@
 import { Selector, t } from "testcafe";
 
-const checkHomeTabContents = async (screenshotPathName:string) => {
+export const checkHomeTabContents = async (screenshotPathName:string) => {
     const navTabs = await Selector('#root').find('div').nth(2);
     const homeTab = navTabs.child('a').nth(0);
     // タブの名前がHomeになっているか確認
@@ -18,5 +18,3 @@ const checkHomeTabContents = async (screenshotPathName:string) => {
         fullPage: true
     });
 };
-
-export default checkHomeTabContents;
