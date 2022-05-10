4bd48bdc3a95f4c8a7459e9a344f7c362f439ba4
azufb <curiousazunas@gmail.com>
2022-05-10 20:36:03 +0900

    テスト追加

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
-import checkHomeTabContents from './testCode/home/checkHomeTabContents';
+import { checkHomeTabContents } from './testCode/home/checkHomeTabContents';
+import { checkButtonsTabContents } from './testCode/buttons/checkButtonsTabContents';
 
-const fixtureName = 'Getting Started';
+const fixtureName: string = 'Getting Started';
 const today = new Date();
 const year = today.getFullYear();
 const month = today.getMonth();
@@ -8,11 +9,16 @@ const date = today.getDate();
 const hour = today.getHours();
 const minute = today.getMinutes();
 const second = today.getSeconds();
-const dateDirectoryName = `${year}-${month}-${date}_${hour}-${minute}-${second}`;
+const dateDirectoryName: string = `${year}-${month}-${date}_${hour}-${minute}-${second}`;
 
 fixture`${fixtureName}`.page`http://localhost:8080`;
 
-test('checkHomeTabContents', async t => {
+test('checkHomeTabContents', async (t: TestController) => {
     const screenshotPathName = `${dateDirectoryName}/checkHomeTabContents.png`;
     await checkHomeTabContents(screenshotPathName);
+});
+
+test('checkButtonsTabContents', async (t: TestController) => {
+    const screenshotPathName = `${dateDirectoryName}/checkButtonsTabContents.png`;
+    await checkButtonsTabContents(screenshotPathName);
 });
