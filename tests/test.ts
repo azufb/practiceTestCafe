cb9cb5babe16b95a7e32ba568ff3f1b9fdd02333
azufb <curiousazunas@gmail.com>
2022-05-12 20:45:45 +0900

    ディレクトリの日付部分修正

--- tests/test.ts
+++ tests/test.ts
@@ -11,50 +11,51 @@ import { checkDisappearedModal } from './testCode/buttons/checkDisappearedModal'
 
 const fixtureName: string = 'TestCaféでE2Eテスト!';
 const pageUrl: string = 'http://localhost:8080';
+const today = new Date();
 
 fixture`${fixtureName}`.page`${pageUrl}`;
 
 test('checkHomeTabContents', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('checkHomeTabContents');
+    const screenshotPathName = createScreenshotDirectory(today, 'checkHomeTabContents');
     await checkHomeTabContents(t, screenshotPathName.singlePathName);
 });
 
 test('checkFormTabContents', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('checkFormTabContents');
+    const screenshotPathName = createScreenshotDirectory(today, 'checkFormTabContents');
     await checkFormTabContents(t, screenshotPathName.singlePathName);
 });
 
 test('checkSubmitButtonDisabled', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('checkSubmitButtonDisabled');
+    const screenshotPathName = createScreenshotDirectory(today, 'checkSubmitButtonDisabled');
     await checkSubmitButtonDisabled(t, screenshotPathName.singlePathName);
 });
 
 test('checkInitialValue', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('checkInitialValue');
+    const screenshotPathName = createScreenshotDirectory(today, 'checkInitialValue');
     await checkInitialValue(t, screenshotPathName.pathDirectory);
 });
 
 test('typeForm', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('typeForm');
+    const screenshotPathName = createScreenshotDirectory(today, 'typeForm');
     await typeForm(t, screenshotPathName.pathDirectory);
 });
 
 test('checkButtonAvailable', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('checkButtonAvailable');
+    const screenshotPathName = createScreenshotDirectory(today, 'checkButtonAvailable');
     await checkButtonAvailable(t, screenshotPathName.singlePathName);
 });
 
 test('checkButtonsTabContents', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('checkButtonsTabContents');
+    const screenshotPathName = createScreenshotDirectory(today, 'checkButtonsTabContents');
     await checkButtonsTabContents(t, screenshotPathName.singlePathName);
 });
 
 test('checkShowModal', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('checkShowModal');
+    const screenshotPathName = createScreenshotDirectory(today, 'checkShowModal');
     await checkShowModal(t, screenshotPathName.singlePathName);
 });
 
 test('checkDisappearedModal', async (t: TestController) => {
-    const screenshotPathName = createScreenshotDirectory('checkDisappearedModal');
+    const screenshotPathName = createScreenshotDirectory(today, 'checkDisappearedModal');
     await checkDisappearedModal(t, screenshotPathName.singlePathName);
 });
--- tests/util/createScreenshotDirectory.ts
+++ tests/util/createScreenshotDirectory.ts
@@ -1,5 +1,4 @@
-export const createScreenshotDirectory = (param: string) => {
-    const today = new Date();
+export const createScreenshotDirectory = (today: Date, param: string) => {
     const year = today.getFullYear();
     const month = (today.getMonth()+1).toString().padStart(2, '0');
     const date = (today.getDate()).toString().padStart(2, '0');
